3aae557bb084a6ba2c9244ecd2a15bdf0a7b7994
bandal0716 <rmsgh64@gmail.com>
2025-04-15 11:35:15 +0900

    Add: `Week7`

---
+++ Week7/app/src/main/java/com/example/week7/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.week7;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textfield;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textfield = (TextView) findViewById(R.id.textview);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(receiver, filter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            textfield.setText(action);
+            if (action.equals(Intent.ACTION_BATTERY_CHANGED)) {
+                int maxvalue = intent.getIntExtra(BatteryManager.EXTRA_SCALE, 0);
+                int value = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+                int level = value * 100 / maxvalue;
+                textfield.setText("현재 배터리 레벨 = " + level);
+            }
+        }
+    };
+}
